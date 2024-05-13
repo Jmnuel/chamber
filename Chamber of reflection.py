c73159f970470a4c556419fd5fa0ee8cb4b18828
Jmnuel <JmanuelIgdalino@gmail.com>
2024-05-14 00:56:05 +0800

    Add files via upload

---
+++ Chamber of reflection.py
@@ -0,0 +1,42 @@
+import time
+
+for v1 in range(4,0,-1):
+    time.sleep(1)
+print("              ")
+print("Spend some time away")
+print("            ")
+
+for v2 in range(3,0,-1):
+    time.sleep(1)
+print("Getting ready for the day you're born again")
+print("            ")
+
+for v3 in range(11,0,-1):
+    time.sleep(1)
+print("Spend some time alone")
+print("            ")
+
+for v4 in range(4,0,-1):
+    time.sleep(1)
+print("Understand that soon you'll run with better men")
+print("            ")
+
+for v5 in range(6,0,-1):
+    time.sleep(1)
+print("Alone againn...")
+print("            ")
+
+for v6 in range(4,0,-1):
+    time.sleep(1)
+print("Alone againnn...")
+print("            ")
+
+for v7 in range(4,0,-1):
+    time.sleep(1)
+print("Alone againnNNnn...")
+print("            ")
+
+for v8 in range(4,0,-1):
+    time.sleep(1)
+print("Alone againnn, Alone")
+print("            ")
